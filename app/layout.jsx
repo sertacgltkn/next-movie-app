72c71218c95a1d68bd2686000c034bc25c890b0e
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-07 10:56:06 +0300

    initial commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,6 @@
-import './globals.css'
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="tr">
       <head />
       <body>{children}</body>
     </html>
