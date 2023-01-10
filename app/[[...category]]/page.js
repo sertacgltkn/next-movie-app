237fde6a5f5a35c3c9510a2d41ccbe54c568fe41
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-10 12:36:56 +0300

    category bugs fixed

--- app/[[...category]]/page.js
+++ app/[[...category]]/page.js
@@ -1,38 +1,17 @@
-import React from "react";
-import { HomeContainer } from "@/containers/home";
+import React from 'react'
+import HomeContainer from '@/containers/home'
+import Movies from '@/mocks/movies.json'
 
-import {
-  fetchPopularMovies,
-  fetchTopRatedMovies,
-  fetchGenres,
-  fetchMoviesByGenre,
-} from "@/services/movie";
-
-async function HomePage({ params }) {
-  const pagePromises = [
-    fetchPopularMovies(),
-    fetchTopRatedMovies(),
-    fetchGenres(),
-  ];
-
-  if (!!params.category?.length) {
-    pagePromises.push(fetchMoviesByGenre(params.category[0]));
+function Homepage({params}) {
+  let selectedCategory;
+  if (params.category?.length > 0) {
+    selectedCategory = true
   }
+    return <HomeContainer selectedCategory={{
+      id: params.category?.[0] ?? '',
+      movies : selectedCategory ? Movies.results.slice(0,7) : [],
+    }}  /> 
 
-  const [popularMovies, topRatedMovies, genres, selectedCategoryMovies] =
-    await Promise.all(pagePromises);
-
-  return (
-    <HomeContainer
-      categories={genres}
-      popularMovies={popularMovies}
-      topRatedMovies={topRatedMovies}
-      selectedCategory={{
-        id: params.category?.[0] ?? "",
-        movies: selectedCategoryMovies ?? [],
-      }}
-    />
-  );
 }
 
-export default HomePage;
+export default Homepage
